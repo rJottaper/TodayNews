537769e60fc122f363f4bc77b0546aac7b5c5c71
matheusaltair <matheusaltair13@gmail.com>
2022-03-17 23:32:41 -0300

    screen Welcome

--- src/Screens/Welcome.js
+++ src/Screens/Welcome.js
@@ -1,10 +1,26 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, Image, View, TouchableOpacity } from 'react-native';
+import welcome from '../../assets/welcome.png';
 
 const Welcome = () => {
+  const navigation = useNavigation() 
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Welcome</Text>
+      <View style={styles.divImagem}>
+        <Image source={ welcome } style={styles.imagem}/>
+      </View>
+      <View style={styles.detalhes}>
+      <Text style={styles.bemVindo}>Bem vindo ao TodayNews</Text>
+      <Text style={styles.descricao}>Aqui você tem acesso a notícias em tempo real</Text>
+      <TouchableOpacity style={styles.botao} onPress={() => navigation.navigate('Home')}>
+        <Text style={styles.textoBotao}>Avançar</Text>
+      </TouchableOpacity>
+
+      </View>
+
+
     </SafeAreaView>
   );
 };
@@ -12,9 +28,39 @@ const Welcome = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: "#ffffff",
+    justifyContent: 'center'
+  },
+  divImagem: {
+    alignItems: 'center',
+  },
+  imagem: {
+    width: 250,
+    height: 250,
+  },
+  detalhes:{
+    paddingHorizontal: 30
+  },
+  bemVindo: {
+    fontSize:36,
+    marginVertical: 20,
+
+  },
+  descricao: {
+    marginVertical: 5
+  },
+  botao: {
+    borderRadius: 8,
+    paddingVertical: 16,
+    backgroundColor: '#098eaf',
+    marginVertical: 30,
     alignItems: 'center'
+  },
+  textoBotao: {
+    color: "white",
+    fontWeight: 'bold'
   }
+
 });
 
 export default Welcome
